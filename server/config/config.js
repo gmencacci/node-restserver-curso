08e16a719c22b0bc4d8e61c3f85e0aeea099f5e8
gmencacci <mencacci_german@hotmail.com>
2019-04-05 12:36:29 -0300

    Creamos variable de entorno para cadena de conexión

--- server/config/config.js
+++ server/config/config.js
@@ -23,7 +23,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://admin:ccp6nE8dWdwUGGs3@cluster0-u1htw.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 
